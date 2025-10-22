4dde011638a1f37cba4dec7c7ed6c0c24292673a
John <rowley.john84@gmail.com>
2025-10-22 11:54:31 +0100

    moving files to doc

--- vite.config.ts
+++ vite.config.ts
@@ -2,8 +2,12 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/dist/",
+  base: "/displacer/",
   plugins: [react()],
+  build: {
+    outDir: "docs",
+    emptyOutDir: true,
+  },
   server: {
     port: 5173,
   },
